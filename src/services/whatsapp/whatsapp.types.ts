0c7488de46c145c1babd279584bf2fa7e751549d
Uchendu Precious <uchendubozz@gmail.com>
2024-11-15 02:43:48 +0100

    chore: log contacts

--- src/services/whatsapp/whatsapp.types.ts
+++ src/services/whatsapp/whatsapp.types.ts
@@ -31,6 +31,7 @@ export type WhatsAppBody = {
         changes: {
             value: {
                 messages: WhatsAppMessagePayload[]
+                contacts: any
             }
         }[]
     }[]
